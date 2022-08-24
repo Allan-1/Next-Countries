befe7650a8ad25b9dfd9fbfc38b3634c50374677
Allan Muturi <amuturi01@gmail.com>
2022-08-24 09:49:08 +0300

    add country search functionality

--- components/searchfilter/Search.js
+++ components/searchfilter/Search.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './search.module.css';
+import { Capitalize } from '../../lib/capitalize';
 import { AiOutlineSearch } from 'react-icons/ai';
 
 function Search() {
+  const [search, setSearch] = useState('');
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(search);
+    location.href = `http://localhost:3000/country/${Capitalize(search)}`;
+  }
   return (
     <div className={styles.search}>
       <div className={styles.input}>
-        <AiOutlineSearch />
-        <input type={'text'} placeholder="search for a country..." />
+        <form onSubmit={handleSubmit}>
+          <AiOutlineSearch />
+          <input
+            type={'text'}
+            placeholder="search for a country..."
+            id="search"
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </form>
       </div>
       <div className={styles.select}>
         <select
